1c70a267eb6ffe42418863d9401db76631f1c16c
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-03 19:30:33 -0300

    Set up all assets for project

---
+++ src/assets/index.js
@@ -0,0 +1,59 @@
+/* ----------------- Illustrations ----------------- */
+
+import heroDesktop from "./illustrations/hero-desktop.png";
+import heroResponsive from "./illustrations/hero-responsive.png";
+import walkthrougOneDesktop from "./illustrations/walkthroug-1-desktop.png";
+import walkthrougOneResonsive from "./illustrations/walkthroug-1-responsive.png";
+import walkthrougTwoDesktop from "./illustrations/walkthroug-2-desktop.png";
+import walkthrougTwoResonsive from "./illustrations/walkthroug-2-responsive.png";
+import walkthrougThreeDesktop from "./illustrations/walkthroug-3-desktop.png";
+import walkthrougThreeResonsive from "./illustrations/walkthroug-3-responsive.png";
+
+/* --------------------- Icons --------------------- */
+
+import aerolabIcon from "./icons/aerolab-icon.svg";
+import arrowDownIcon from "./icons/arrow-down-icon.svg";
+import arrowNextIcon from "./icons/arrow-next-icon.svg";
+import browseIcon from "./icons/browse-icon.svg";
+import chooseIcon from "./icons/choose-icon.svg";
+import closeIcon from "./icons/close-icon.svg";
+import enjoyIcon from "./icons/enjoy-icon.svg";
+import errorIcon from "./icons/error-icon.svg";
+import githubIcon from "./icons/github-icon.svg";
+import successIcon from "./icons/success-icon.svg";
+
+/* --------------------- Logos --------------------- */
+
+import aerolabLogo from './logos/aerolab-logo.png';
+import aerolabLogoComplete from './logos/aerolab-logo-complete.png';
+
+/* -------------------- Exports -------------------- */
+
+export const illustrations = {
+  heroDesktop,
+  heroResponsive,
+  walkthrougOneDesktop,
+  walkthrougOneResonsive,
+  walkthrougTwoDesktop,
+  walkthrougTwoResonsive,
+  walkthrougThreeDesktop,
+  walkthrougThreeResonsive,
+};
+
+export const icons = {
+  aerolabIcon,
+  arrowDownIcon,
+  arrowNextIcon,
+  browseIcon,
+  chooseIcon,
+  closeIcon,
+  enjoyIcon,
+  errorIcon,
+  githubIcon,
+  successIcon,
+};
+
+export const logos = {
+  aerolabLogo,
+  aerolabLogoComplete
+}
