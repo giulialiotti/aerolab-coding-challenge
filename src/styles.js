48eace227fcafd1dcb04e1df11bec93085e5849d
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-03 20:16:09 -0300

    Added colors and gradients

---
+++ src/styles.js
@@ -0,0 +1,38 @@
+export const styles = {
+  colors: {
+    neutrals: {
+      nine: "#252F3D",
+      six: "#7C899C",
+      five: "#8FA3BF",
+      three: "#DAE4F2",
+      two: "#E6EDF7",
+      one: "#F5F9FF",
+      cero: "#FFFFFF",
+    },
+    brand: {
+      light: "#E5F0FF",
+      light2: "#CCE1FF",
+    },
+    green: {
+      default: "#29CC74",
+      light: "#CCFFE3",
+    },
+    red: {
+      default: "#E07F4F",
+      light: "#FFDFD9",
+    },
+  },
+  gradients: {
+    brand: {
+      default: "linear-gradient(102.47deg, #176FEB -5.34%, #FF80FF 106.58%)",
+      hover: "linear-gradient(102.47deg, #1667D9 -5.34%, #F279F2 106.58%)",
+    },
+    specials: {
+      sectionBg:
+        "linear-gradient(102.47deg, rgba(23, 111, 235, 0.5) -5.34%, rgba(255, 128, 255, 0.5) 106.58%)",
+      illustrationBg:
+        "linear-gradient(102.47deg, #7296EB -5.34%, #EAC0E9 106.58%, #EAC0E9 106.58%)",
+      aerolab: "linear-gradient(180deg, #FF8800 0%, #FF6600 100%)",
+    },
+  },
+};
