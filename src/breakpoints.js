b7eb3366f32efb59187c17b4291a5c67fc3ee045
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-05 11:25:43 -0300

    Added breakpoints variables

---
+++ src/breakpoints.js
@@ -0,0 +1,9 @@
+const mobile = "@media screen and (max-width: 749px)";
+const tablet = "@media screen and (min-width: 750px)";
+const desktop = "@media screen and (min-width: 1900px)";
+
+export const breakpoints = {
+  mobile,
+  tablet,
+  desktop,
+};
