9168e75b042bed5428bc8ff8c1fe100dfa356ef4
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-05 11:27:25 -0300

    Added breakpoints styles to typographys

--- src/typographys.js
+++ src/typographys.js
@@ -1,70 +1,103 @@
 import styled from "styled-components";
+import { breakpoints } from "./breakpoints";
 
 /* -------- TITLES | Desktop -------- */
 
 const TitleL1 = styled.h1`
   font-family: "Montserrat", sans-serif;
-  font-size: 200px;
+  font-size: 96px;
   font-weight: 900;
   line-height: 80%;
   text-transform: uppercase;
+
+  ${breakpoints.desktop} {
+    font-size: 200px;
+  } 
 `;
 
 const TitleL2 = styled.h2`
   font-family: "Montserrat", sans-serif;
-  font-size: 48px;
+  font-size: 32px;
   font-weight: 900;
   line-height: 80%;
   text-transform: uppercase;
+
+  ${breakpoints.desktop} {
+    font-size: 48px;
+  } 
 `;
 
 const TitleL3 = styled.h3`
   font-family: "Montserrat", sans-serif;
-  font-size: 32px;
+  font-size: 24px;
   font-weight: 900;
   line-height: 100%;
   text-transform: uppercase;
+
+  ${breakpoints.desktop} {
+    font-size: 32px;
+  } 
 `;
 
 /* -------- TEXTS | Desktop -------- */
 
 const TextL1 = styled.p`
   font-family: "Montserrat", sans-serif;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 600;
   line-height: 150%;
+
+  ${breakpoints.desktop} {
+    font-size: 18px;
+  } 
 `;
 
 const TextL1AllCaps = styled.p`
   font-family: "Montserrat", sans-serif;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 600;
   line-height: 150%;
   letter-spacing: 24%;
   text-transform: uppercase;
+
+  ${breakpoints.desktop} {
+    font-size: 18px;
+  } 
 `;
 
 const TextL1Light = styled.p`
   font-family: "Montserrat", sans-serif;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 500;
   line-height: 150%;
+
+  ${breakpoints.desktop} {
+    font-size: 18px;
+  } 
 `;
 
 const TextL2 = styled.p`
   font-family: "Montserrat", sans-serif;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 600;
   line-height: 150%;
+
+  ${breakpoints.desktop} {
+    font-size: 14px;
+  } 
 `;
 
 const TextL2AllCaps = styled.p`
   font-family: "Montserrat", sans-serif;
-  font-size: 14px;
+  font-size: 12 px;
   font-weight: 600;
   line-height: 150%;
-  letter-spacing: 5%;
   text-transform: uppercase;
+
+  ${breakpoints.desktop} {
+    font-size: 14px;
+    letter-spacing: 5%;
+  } 
 `;
 
 export const typographys = {
