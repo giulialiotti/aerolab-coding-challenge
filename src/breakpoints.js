9924656b5006b490590b421589ce85fa839321fe
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-05 16:31:15 -0300

    Touch up breakpoint variable

--- src/breakpoints.js
+++ src/breakpoints.js
@@ -1,6 +1,6 @@
 const mobile = "@media screen and (max-width: 749px)";
 const tablet = "@media screen and (min-width: 750px)";
-const desktop = "@media screen and (min-width: 1900px)";
+const desktop = "@media screen and (min-width: 1920px)";
 
 export const breakpoints = {
   mobile,
