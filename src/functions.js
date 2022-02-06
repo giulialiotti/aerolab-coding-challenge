3c254341e32e21b5ea1fcc5b67412313b54523ac
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-06 13:24:37 -0300

    Change fetch api function to be reusable

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,8 @@
-const url = "https://coding-challenge-api.aerolab.co/products";
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZlOWRhY2Q0MDc0YzAwMjE0M2RkZmQiLCJpYXQiOjE2NDQwNzY0NjB9.qCiQ-2Lx8p3-2ynQR3a7t6FUSLKXHPvWOm6BnvTsbaI";
 
-// Fetch products from API 
-export async function getProducts() {
+// Reusable function to get data from endpoints
+export async function getData(url) {
   try {
     let response = await fetch(url, {
       method: "GET",
