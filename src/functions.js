c962ec7c43847ecf70b46d33d27d444de14b2605
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-02-06 19:53:57 -0300

    Change addPoints to be reused by post requests

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,6 @@
 
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZlOWRhY2Q0MDc0YzAwMjE0M2RkZmQiLCJpYXQiOjE2NDQwNzY0NjB9.qCiQ-2Lx8p3-2ynQR3a7t6FUSLKXHPvWOm6BnvTsbaI";
-const addPointsUrl = "https://coding-challenge-api.aerolab.co/user/points";
 
 /* ---------------- FUNCTIONS ---------------- */
 
@@ -31,17 +30,17 @@ export function sortProducts(a, b) {
   return a.cost - b.cost;
 }
 
-// Add points to user
-export async function addPoints(amount) {
+// Add items to user (used for adding points, or claiming products)
+export async function addItem(item, url) {
   try {
-    let response = await fetch(addPointsUrl, {
+    let response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
         Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify(amount)
+      body: JSON.stringify(item)
     });
     let data = await response.json();
 
